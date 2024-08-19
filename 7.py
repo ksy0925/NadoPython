e27a349205ed1035234655224048131e1e388ad4
hirune <ksy092520@gmail.com>
2024-08-19 12:50:26 +0900

    test

---
+++ 7.py
@@ -0,0 +1,8 @@
+station = "사당"
+print(station + "행" + " 열차가 들어오고 있습니다.")
+
+station2 = "신도림"
+print(station2 + "행" + " 열차가 들어오고 있습니다.")
+
+station3 = "인천공항"
+print(station3 + "행" + " 열차가 들어오고 있습니다.")
---
+++ 8.py
@@ -0,0 +1,11 @@
+status = "상품 준비"
+
+print("주문상태 : " + status)
+
+status2 = "배송 중"
+
+print("주문상태 : " + status2)
+
+status3 = "배송 완료"
+
+print("주문상태 : " + status3)
