97d83c04bdcb8ac719f6a3e4a54268c812c7f1b9
hirune <ksy092520@gmail.com>
2024-09-01 19:00:54 +0900

    test

---
+++ 11.py
@@ -0,0 +1,25 @@
+number = 2 + 3 * 4 
+print(number)
+
+number = 2 + 3 * 4 + 2
+print(number)
+
+number = number + 2
+print(number)
+
+number += 2
+print(number)
+
+
+number -= 2
+print(number)
+number *= 2
+print(number)
+number /= 2
+print(number)
+number **= 2
+print(number)
+number //= 2
+print(number)
+number %= 2
+print(number)
---
+++ 13.py
@@ -0,0 +1,14 @@
+from random import *
+
+# print(random())
+# print(random())
+# print(random())
+
+
+# print(random() * 10)
+# print(int(random() * 10))
+# print(int(random() * 10 + 1))
+
+
+print(int(random() * 45) + 1)
+
